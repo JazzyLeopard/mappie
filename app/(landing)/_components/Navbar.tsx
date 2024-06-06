9be48d7b3b7451acd1a23ef5326ffb9fb72d6948
Yash Soni <yash.soni2737@gmail.com>
2024-06-07 00:05:56 +0530

    fixed convex and breaking pages

--- app/(landing)/_components/Navbar.tsx
+++ app/(landing)/_components/Navbar.tsx
@@ -1,51 +1,49 @@
 "use client";
 
-import { useScrollTop } from "@/Hooks/use-scroll-top";
+// import { useScrollTop } from "@/Hooks/use-scroll-top";
 import { cn } from "@/lib/utils";
 import { Logo } from "./Logo";
-import { SignInButton, UserButton } from "@clerk/clerk-react";
-import { Button } from "@/components/ui/button";
-import { Spinner } from "@/components/spinner";
+import {
+	SignInButton,
+	UserButton,
+} from "@clerk/clerk-react";
+// import { Button } from "@/components/ui/button";
+// import { Spinner } from "@/components/spinner";
 import Link from "next/link";
 
 export const Navbar = () => {
-    const scrolled = useScrollTop();
+	// const scrolled = useScrollTop();
 
-    return (
-        <div className={cn("z-50 bg-background fixed top-0 flex items-center w-full p-6",
-        scrolled && "border-b shadow-sm")}>
-            <Logo />
-            <div className="md:ml-auto md:justify-end justify-between w-full flex items-center gap-x-2">
-                    <Spinner />
-                
-                (
-                    <>
-                    <SignInButton mode="modal">
-                        <Button variant="ghost" size="sm">
-                            Log in
-                        </Button>
-                    </SignInButton>
-                    <SignInButton mode="modal">
-                        <Button size="sm">
-                            Register
-                        </Button>
-                    </SignInButton>
-                    </>
-                )
-                <>
-                    <Button variant="ghost" size="sm" asChild>
-                        <Link href="/projects">
-                            Enter Listoriq
-                        </Link>
-                    </Button>
-                    <UserButton afterSignOutUrl="/" 
-                    />
-                    </>
-                
-            </div>
-        </div>
-    )
-}
+	return (
+		<></>
+		// <div
+		// 	className={cn(
+		// 		"z-50 bg-background fixed top-0 flex items-center w-full p-6",
+		// 		scrolled && "border-b shadow-sm"
+		// 	)}>
+		// 	<Logo />
+		// 	<div className="md:ml-auto md:justify-end justify-between w-full flex items-center gap-x-2">
+		// 		<Spinner />(
+		// 		<>
+		// 			<SignInButton mode="modal">
+		// 				<Button variant="ghost" size="sm">
+		// 					Log in
+		// 				</Button>
+		// 			</SignInButton>
+		// 			<SignInButton mode="modal">
+		// 				<Button size="sm">Register</Button>
+		// 			</SignInButton>
+		// 		</>
+		// 		)
+		// 		<>
+		// 			<Button variant="ghost" size="sm" asChild>
+		// 				<Link href="/projects">Enter Listoriq</Link>
+		// 			</Button>
+		// 			<UserButton afterSignOutUrl="/" />
+		// 		</>
+		// 	</div>
+		// </div>
+	);
+};
 
 export default Navbar;
-
