09b038ea699db9e52403d15c37bdb804386c7f5a
Yash Soni <yash.soni2737@gmail.com>
2024-06-07 00:06:06 +0530

    fixed convex

--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -1,42 +1,56 @@
-'use client';
+"use client";
 
-import Image from 'next/image';
+import Image from "next/image";
 import { useUser } from "@clerk/clerk-react";
 import { PlusCircle } from "lucide-react";
-import { Button } from "@/components/ui/button";
-import { useMutation } from "convex/react";
-import { api } from "@/convex/_generated/api";
-import { toast } from "sonner";
+// import { Button } from "@/components/ui/button";
+// import { useMutation } from "convex/react";
+// import { api } from "@/convex/_generated/api";
+// import { toast } from "sonner";
 
 const ProjectsPage = () => {
-    const { user } = useUser();
-    const createProject = useMutation(api.projects.createProject);
+	// const { user, isSignedIn } = useUser();
 
-    const onCreate = () => {
-        const promise = createProject({title: "Untitled", userId: user?.id, isPublished: false, description: ""});
-        
-        toast.promise(promise, {
-            loading: "Creating project...",
-            success: "New project created",
-            error: "Failed to create project"
-        });
-    }
+	// if (!isSignedIn) {
+	// 	return <h1>Not logged in</h1>;
+	// }
+	// const createProject = useMutation(
+	// 	api.projects.createProject
+	// );
 
-    return ( 
-        <div className="h-full flex flex-col items-center justify-center space-y-6">
-            <Image 
-                src="/empty.svg" 
-                alt="documents" 
-                width={200} 
-                height={200} 
-            />
-            <h2 className="text-xl font-semibold">Welcome to {user?.firstName}&apos;s DictaDoc</h2>
-            <Button onClick={onCreate}>
-                <PlusCircle className="h-4 w-4 mr-2" />
-                <p>Create a new project</p>
-            </Button>
-        </div>
-        );
-    };
+	// const onCreate = () => {
+	// 	const promise = createProject({
+	// 		title: "Untitled",
+	// 		userId: user?.id,
+	// 		isPublished: false,
+	// 		description: "",
+	// 	});
+
+	// 	toast.promise(promise, {
+	// 		loading: "Creating project...",
+	// 		success: "New project created",
+	// 		error: "Failed to create project",
+	// 	});
+	// };
+
+	return (
+		<div className="h-full flex flex-col items-center justify-center space-y-6">
+			{/* <Image
+				src="/empty.svg"
+				alt="documents"
+				width={200}
+				height={200}
+			/> */}
+			Hello World
+			{/* <h2 className="text-xl font-semibold">
+				Welcome to {user?.firstName}&apos;s DictaDoc
+			</h2>
+			<button onClick={onCreate}>
+				<PlusCircle className="h-4 w-4 mr-2" />
+				<p>Create a new project</p>
+			</button> */}
+		</div>
+	);
+};
 
 export default ProjectsPage;
--- app/(main)/(routes)/projects/[projectId]/page.tsx
+++ app/(main)/(routes)/projects/[projectId]/page.tsx
@@ -1,42 +1,35 @@
-"use client"
+"use client";
 
-import { useQuery } from "convex/react"
+// import { useQuery } from "convex/react";
 
-import { Id } from "@/convex/_generated/dataModel"
-import { api } from "@/convex/_generated/api"
+// import { Id } from "@/convex/_generated/dataModel";
+// import { api } from "@/convex/_generated/api";
 
 interface ProjectIdPageProps {
-  params: {
-    projectId: Id<"projects">
-  }
+	// params: {
+	// 	projectId: Id<"projects">;
+	// };
 }
 
-const ProjectIdPage  = ({ params }: ProjectIdPageProps) => {
-  const project = useQuery(api.projects.getById, { projectId: params.projectId });
+const ProjectIdPage = ({}: // params
+ProjectIdPageProps) => {
+	// const project = useQuery(api.projects.getById, {
+	// 	projectId: params.projectId,
+	// });
 
-  if (project === undefined) {
-    return (
-      <div>
-        Loading...
-      </div>
-    )
-  }
+	// if (project === undefined) {
+	// 	return <div>Loading...</div>;
+	// }
 
-  if (project === null) {
-    return (
-      <div>
-        Project not found
-      </div>
-    )
-  }
+	// if (project === null) {
+	// 	return <div>Project not found</div>;
+	// }
 
-
-  return (
-    <div className="pb-40">
-      <div className="md:max-w-3xl lg:max-w-4xl mx-auto">
-      </div>
-    </div>
-  );
+	return (
+		<div className="pb-40">
+			<div className="md:max-w-3xl lg:max-w-4xl mx-auto"></div>
+		</div>
+	);
 };
 
 export default ProjectIdPage;
