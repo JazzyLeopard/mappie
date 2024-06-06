2cbdae3ed4e233ae06fcd338741fdd7d8ac40dd1
Yash Soni <yash.soni2737@gmail.com>
2024-06-07 00:05:56 +0530

    fixed convex and breaking pages

--- app/(landing)/_components/Logo.tsx
+++ app/(landing)/_components/Logo.tsx
@@ -1,13 +1,14 @@
 import Image from "next/image";
 
 export const Logo = () => {
-    return (
-        <div className="hidden md:flex items-center gap-x-2">
-            <Image 
-                src="/logolis.svg" 
-                alt="Logo" 
-                width="100" height="100" />
-        </div>
-    )
-}
-
+	return (
+		<div className="hidden md:flex items-center gap-x-2">
+			<Image
+				src="/logolis.svg"
+				alt="Logo"
+				width="100"
+				height="100"
+			/>
+		</div>
+	);
+};
