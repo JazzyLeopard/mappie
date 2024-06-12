7582898f0cd3e5c5a98761718fbaab91db84f7bc
Yash Soni <yash.soni2737@gmail.com>
2024-06-12 23:59:32 +0530

    Added collapse functionality

--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -6,9 +6,15 @@ import {
 	MenuIcon,
 	MenuSquareIcon,
 } from "lucide-react";
+import { Ewert } from "next/font/google";
 import { usePathname } from "next/navigation";
 
-import { ElementRef, useRef, useState } from "react";
+import {
+	ElementRef,
+	useEffect,
+	useRef,
+	useState,
+} from "react";
 import { useMediaQuery } from "usehooks-ts";
 
 export const Navigation = () => {
@@ -25,7 +31,22 @@ export const Navigation = () => {
 
 	// If mobile view it will auto matically collapse "true"
 	const [isCollapsed, setIsCollapsed] = useState(isMobile);
-	console.log(isCollapsed);
+
+	useEffect(() => {
+		if (isMobile) {
+			collapse();
+		} else {
+			resetWidth();
+		}
+	}, [isMobile]);
+
+	useEffect(() => {
+		if (isMobile) {
+			collapse();
+		} else {
+			resetWidth();
+		}
+	}, [pathname, isMobile]);
 
 	// WHEN NAVBAR IS MOVED
 	const handleMouseMove = (event: MouseEvent) => {
@@ -103,6 +124,21 @@ export const Navigation = () => {
 		setTimeout(() => setIsResetting(false), 300);
 	};
 
+	const collapse = () => {
+		if (sidebarRef.current && navbarRef.current) {
+			setIsCollapsed(true);
+			setIsResetting(true);
+		}
+
+		sidebarRef.current!.style.width = "0";
+
+		navbarRef.current!.style.setProperty("width", "100%");
+
+		navbarRef.current!.style.setProperty("left", "0");
+
+		setTimeout(() => setIsResetting(false), 300);
+	};
+
 	return (
 		<>
 			<aside
@@ -116,6 +152,7 @@ export const Navigation = () => {
 				<div
 					title="Collapsible sidebar"
 					role="button"
+					onClick={collapse}
 					className={cn(
 						"h-6 w-6 text-muted-foreground hover:bg-neutral-300 rounded-sm absolute top-3 right-1 opacity-0 group-hover/sidebar:opacity-100",
 						isMobile && "opacity-100"
@@ -128,6 +165,7 @@ export const Navigation = () => {
 				<div className="mt-4">Documents</div>
 
 				<div
+					onClick={resetWidth}
 					onMouseDown={handleMouseDown}
 					className="opacity-0 group-hover/sidebar:opacity-100 transition
                                 cursor-ew-resize absolute h-full w-1 bg-primary/10 right-0 top-0"
@@ -145,6 +183,7 @@ export const Navigation = () => {
 				<nav className="bg-transparent w-full px-3 py-2">
 					{isCollapsed && (
 						<MenuSquareIcon
+							onClick={resetWidth}
 							role="button"
 							className="h-6 w-6 text-muted-foreground"
 						/>
