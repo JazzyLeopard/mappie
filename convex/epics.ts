640d5cd1f2b28bf58c80a7a1b02214eaee2b54b2
Yash Soni <yash.soni2737@gmail.com>
2024-06-12 23:59:32 +0530

    Added collapse functionality

--- convex/epics.ts
+++ convex/epics.ts
@@ -26,7 +26,8 @@ export const createUserStory = mutation({
 			functionalFlow: args.functionalFlow,
 			createdAt: BigInt(Date.now()),
 			updatedAt: BigInt(Date.now()),
-		});
+        });
+
 		return userStoryId;
 	},
 });
