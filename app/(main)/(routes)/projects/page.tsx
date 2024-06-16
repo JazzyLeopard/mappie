2e21721b186aadc796d6d90fe64965e0d039528e
Yash Soni <yash.soni2737@gmail.com>
2024-06-16 19:46:27 +0530

    Implemented create and get all projects from db, rendered projects in the side bar added new project button and check for no projects of the user

--- app/(main)/(routes)/projects/page.tsx
+++ app/(main)/(routes)/projects/page.tsx
@@ -1,63 +1,64 @@
 "use client";
 
 import Image from "next/image";
-import { SignedIn, UserButton } from "@clerk/nextjs";
 import { useUser } from "@clerk/clerk-react";
 import { PlusCircle } from "lucide-react";
 import { Button } from "@/components/ui/button";
-import { useClerk } from "@clerk/clerk-react";
-import { useEffect } from "react";
 import Empty from "@/public/empty.png";
 
-import { useMutation } from "convex/react";
+import { useMutation, useQuery } from "convex/react";
 import { api } from "@/convex/_generated/api";
 import { toast } from "sonner";
-import { createProject } from "@/convex/projects";
 
 const ProjectsPage = () => {
 	const { user, isSignedIn } = useUser();
 
-	// const createProject = useMutation(
-	// 	api.projects.createProject
-	// );
+	const projects = useQuery(api.projects.getProjects);
 
-	// const onCreate = () => {
-	// 	const promise = createProject({
-	// 		title: "Untitled",
-	// 		userId: user?.id,
-	// 		isPublished: false,
-	// 		description: "",
-	// 	});
+	const createProject = useMutation(
+		api.projects.createProject
+	);
 
-	// 	toast.promise(promise, {
-	// 		loading: "Creating project...",
-	// 		success: "New project created",
-	// 		error: "Failed to create project",
-	// 	});
-	// };
+	const onCreate = () => {
+		const promise = createProject({
+			title: "Untitled Project",
+		});
+
+		toast.promise(promise, {
+			loading: "Creating new project...",
+			success: "New project created",
+			error: "Failed to create project",
+		});
+	};
 
 	return (
-		<div className="h-full w-full flex flex-col items-center justify-center space-y-6">
-			<Image
-				src={Empty}
-				alt="documents"
-				width={100}
-				height={100}
-			/>
+		<>
+			<div className="h-full w-full flex flex-col items-center justify-center space-y-6">
+				{projects?.length === 0 && (
+					<>
+						<h1 className="text-3xl font-semibold mb-8">
+							{user?.firstName}&apos;s space
+						</h1>
 
-			<h2 className="text-xl font-semibold">
-				Welcome to {user?.fullName}&apos;s Listoriq
-			</h2>
+						<Image
+							src={Empty}
+							alt="documents"
+							width={100}
+							height={100}
+						/>
 
-			<SignedIn>
-				<UserButton afterSignOutUrl="/" />
-			</SignedIn>
+						<h2 className="text-xl font-semibold">
+							You haven’t created any projects...
+						</h2>
 
-			<Button onClick={() => {}}>
-				<PlusCircle className="h-4 w-4 mr-2 " />
-				<p>Create a new project</p>
-			</Button>
-		</div>
+						<Button variant="default" onClick={onCreate}>
+							<PlusCircle className="h-4 w-4 mr-2 " />
+							<p>Create your first project</p>
+						</Button>
+					</>
+				)}
+			</div>
+		</>
 	);
 };
 
