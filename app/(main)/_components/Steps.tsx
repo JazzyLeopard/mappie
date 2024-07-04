3300a1e21b038be1a3ba8da4bc390004a71a4286
Yash Soni <yash.soni2737@gmail.com>
2024-07-04 17:44:08 +0530

    Data update on blur

--- app/(main)/_components/Steps.tsx
+++ app/(main)/_components/Steps.tsx
@@ -1,53 +1,94 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useRouter } from 'next/navigation';
 import {
   Card,
   CardHeader,
   CardTitle,
   CardDescription,
-  CardFooter
+  CardFooter,
+  CardContent
 } from "@/components/ui/card";
-import { ChevronLeft, ChevronRight } from "lucide-react";
+import { ChevronLeft, ChevronRight, Component } from "lucide-react";
 import { Input } from '@/components/ui/input';
 import { Button } from '@/components/ui/button';
 import { CKEditor } from '@ckeditor/ckeditor5-react';
 import InlineEditor from '@ckeditor/ckeditor5-build-inline';
-import AdditionalSteps from './AdditionalSteps'; // Import your AdditionalSteps component here
+import AdditionalSteps from './AdditionalSteps';
 import { useMutation } from 'convex/react';
 import { api } from '@/convex/_generated/api';
 
-const Steps = ({ project }: { project: any }) => {
+
+const steps = [
+  {
+    title: 'What is the name of your project?',
+    key: 'title',
+    placeholder: 'Project Name',
+    stepNumber: 1
+  },
+  {
+    title: 'Can you describe your project in a few sentences?',
+    key: 'description',
+    placeholder: 'Write a description',
+    stepNumber: 2
+  },
+  {
+    title: 'What are the main objectives of this project?',
+    key: "objectives",
+    placeholder: 'Specify Objectives',
+    stepNumber: 3
+  },
+  {
+    title: 'What is in scope? What is out of scope?',
+    key: "scope",
+    placeholder: 'Specify Scope',
+    stepNumber: 4
+  },
+  {
+    title: 'Who are the main stakeholders? Who is involved in this project?',
+    key: "stakeholders",
+    placeholder: 'Specify Stakeholders',
+    stepNumber: 5
+  },
+  {
+    key:'additional steps',
+    stepNumber: 6
+  }
+];
+
+const Steps = ({ project }:{project:any}) => {
   const router = useRouter();
   const [step, setStep] = useState(project.onboarding);
-  const [inputValue, setInputValue] = useState('');
   const [projectDetails, setProjectDetails] = useState(project);
-  const [showAdditionalSteps, setShowAdditionalSteps] = useState(false); // State to manage showing additional steps
+  const [currentStep, setCurrentStep] = useState(steps[0]);
 
   const updateProjectMutation = useMutation(api.projects.updateProject);
 
-  const handleContinue = () => {
-      // Implement your logic for handling step transitions
-      if (step == 5) {
-        setShowAdditionalSteps(true); // Show additional steps if on the last step
-    } else {
-        setStep(step + 1);
+  useEffect(() => {
+    const foundStep = steps.find(s => s.stepNumber === step);
+    if (foundStep) {
+      setCurrentStep(foundStep);
     }
+  }, [step]);
+
+  const handleContinue = () => {
+    console.log("Additional button clicked")
+    if (step !== 6) {
+      setStep(step + 1);
+    } 
   };
 
   const handleBack = () => {
-   // Implement your logic for handling step transitions
-   if (step > 1) {
-    setStep(step - 1);
-    setShowAdditionalSteps(false); // Reset to hide additional steps if navigating back
-    }
-    else {
-        router.push(`/project/${project.id}/overview`);
-    // navigate
+    console.log('handleBack called');
+     if (step > 1) {
+      setStep(step - 1);
+    } else {
+      router.push(`/project/${project.id}/overview`);
     }
   };
 
-  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setInputValue(e.target.value);
+  const handleInputChange = (e: any) => {
+    const { value } = e.target;
+    setProjectDetails({ ...projectDetails, title: value });
   };
 
   const handleEditorChange = (event: any, editor: { getData: () => any }, key: string) => {
@@ -55,66 +96,43 @@ const Steps = ({ project }: { project: any }) => {
     setProjectDetails({ ...projectDetails, [key]: data });
   };
 
-
-
-  const steps = [
-    {
-      title: 'What is the name of your project?',
-      key: 'title',
-      placeholder: 'Project Name',
-      stepNumber: 1
-    },
-    {
-      title: 'Can you describe your project in a few sentences?',
-      key: 'description',
-      placeholder: 'Write a description',
-      stepNumber: 2
-    },
-    {
-      title: 'What are the main objectives of this project?',
-      key: "objectives",
-      placeholder: 'Specify Objectives',
-      stepNumber: 3
-    },
-    {
-      title: 'What is in scope? What is out of scope?',
-      key: "scope",
-      placeholder: 'Specify Scope',
-      stepNumber: 4
-    },
-    {
-      title: 'Who are the main stakeholders? Who is involved in this project?',
-      key: "stakeholders",
-      placeholder: 'Specify Stakeholders',
-      stepNumber: 5
-    },
-
-  ];
+  const onEditorBlur = async () => {
+    try {
+      console.log('time for API call', projectDetails);
+      const { _creationTime, createdAt, updatedAt, userId, ...payload } = projectDetails
+      payload.onboarding = step
+      await updateProjectMutation(payload)
+    } catch (error) {
+      console.log('error updating project', error);
+    }
+  };
 
   return (
     <>
-        {!showAdditionalSteps ?
-        (
-            <Card className='mt-8 flex flex-col justify-items-start'>
+      {step !== 6 &&  (
+        <Card className='mt-8 flex flex-col justify-items-start'>
           <CardHeader>
-            <CardTitle>{steps[step - 1]?.title}</CardTitle>
+            <CardTitle>{currentStep?.title}</CardTitle>
           </CardHeader>
-          <CardDescription className='w-full'>
-          {step === 1 && (
-              <Input className='m-4 w-[95%]'
-                value={inputValue}
-                placeholder={steps[step - 1].placeholder}
+          <CardContent className='w-full'>
+            {step === 1 && (
+              <Input className='w-full mt-2'
+                value={projectDetails.title}
+                placeholder={currentStep?.placeholder}
                 onChange={handleInputChange}
+                onBlur={()=>{onEditorBlur()}}
               />
             )}
             {step >= 2 && step <= 5 && (
-              <div className='prose max-w-none m-6 p-4 outline outline-slate-200'>
+              <div className='prose max-w-none'>
                 <CKEditor
+                  key={step} // Use step as key to force re-render
                   editor={InlineEditor}
-                  data={projectDetails[steps[step - 1]?.key] || ''}
-                  onChange={(event, editor) => handleEditorChange(event, editor, steps[step - 1].key)}
+                  data={projectDetails[currentStep.key] || ''}
+                  onBlur={() => onEditorBlur()}
+                  onChange={(event, editor) => handleEditorChange(event, editor, currentStep.key)}
                   config={{
-                    placeholder: steps[step - 1].placeholder,
+                    placeholder: steps[step-1].placeholder,
                     toolbar: [
                       'bold',
                       'italic',
@@ -131,29 +149,25 @@ const Steps = ({ project }: { project: any }) => {
                   }}
                 />
               </div>
-            )}  
-            {step === 6 && (
-              <AdditionalSteps project={project} onAdditionalBackClick={handleBack} />
             )}
-          </CardDescription>
-          <CardFooter className={`mt-2 flex ${step === 1 ? 'justify-end mt-6' : 'justify-between'}`}>
+          </CardContent>
+          <CardFooter className={`mt-2 flex ${step === 1 ? 'justify-end' : 'justify-between'}`}>
             {step > 1 && (
               <Button variant="outline" onClick={handleBack}>
                 <ChevronLeft className="h-4 w-4 ml-1" />
                 Back
               </Button>
             )}
-            <Button onClick={handleContinue}>
+            <Button onClick={handleContinue}> 
               {step === 5 ? 'View Additional Steps' : 'Continue'}
               <ChevronRight className="h-4 w-4 ml-1" />
             </Button>
           </CardFooter>
         </Card>
-        ) :(
-            <AdditionalSteps project={project} onAdditionalBackClick={handleBack} />
-        )}
+      )}
+      {step >= 6 && <AdditionalSteps project={project} onBackClick={handleBack} />}
     </>
   );
-};
+}
 
 export default Steps;
