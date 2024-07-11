ff7b1fb8dd9f6028ba1944e3801235442bf068cc
Yash Soni <yash.soni2737@gmail.com>
2024-07-11 15:48:29 +0530

    sidebar fix as per UI

--- app/(main)/(routes)/projects/[projectId]/epics/page.tsx
+++ app/(main)/(routes)/projects/[projectId]/epics/page.tsx
@@ -1,6 +1,41 @@
+'use client'
 
-const EpicsPage = () => {
-    return (<h1>Epics Page</h1>)
+import { Button } from "@/components/ui/button"
+import { api } from "@/convex/_generated/api"
+import { Id } from "@/convex/_generated/dataModel"
+import { useMutation } from "convex/react"
+import { toast } from "sonner"
+
+interface EpicsPageProps {
+    params: {
+        projectId: Id<"projects">
+    }
+}
+
+const EpicsPage = ({ params }: EpicsPageProps) => {
+
+    const id = params.projectId
+
+    const createEpic = useMutation(api.epics.createEpics)
+
+    const onCreateEpic = (projectId: Id<"projects">) => {
+        const mypromise = createEpic({
+            title: "Untitled Epic",
+            projectId: projectId,
+        });
+
+        toast.promise(mypromise, {
+            loading: "Creating new Epic...",
+            success: "New Epic created",
+            error: "Failed to create Epic",
+        });
+    }
+
+    return (
+        <>
+            <Button className="mt-40" onClick={() => onCreateEpic(id)}>Create Epic</Button>
+        </>
+    )
 }
 
 export default EpicsPage
