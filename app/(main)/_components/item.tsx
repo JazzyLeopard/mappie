c4521021edc417c490086fbfb84b51c87ccea871
Yash Soni <yash.soni2737@gmail.com>
2024-07-11 15:48:29 +0530

    sidebar fix as per UI

--- app/(main)/_components/item.tsx
+++ app/(main)/_components/item.tsx
@@ -1,25 +1,142 @@
-import { LucideIcon } from "lucide-react";
-import React from "react";
+import { ChevronDown, ChevronRight, LucideIcon, MoreHorizontal, PlusIcon } from "lucide-react";
+import React, { useState } from "react";
+
+import { Id } from "@/convex/_generated/dataModel";
+import { cn } from "@/lib/utils";
+import { useMutation, useQuery } from "convex/react";
+import { api } from "@/convex/_generated/api";
+import { toast } from "sonner";
+
+import { Button } from "@/components/ui/button";
+import { DialogHeader, DialogFooter, Dialog, DialogContent, DialogDescription, DialogTitle } from "@/components/ui/dialog";
+import { useRouter } from "next/navigation";
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
 
 interface ItemProps {
-	label: string;
-	icon: LucideIcon;
-	onClick: () => void;
+	id?: Id<"projects">;
+	active?: boolean;
+	expanded?: boolean;
+	level?: number;
+	onExpand?: () => void;
+	label?: string;
+	icon: LucideIcon | null;
+	onClick?: () => void;
 }
 
 const Item = ({
+	id,
+	active,
+	expanded,
+	level = 0,
+	onExpand,
 	label,
 	onClick,
 	icon: Icon,
 }: ItemProps) => {
+	const router = useRouter()
+	const create = useMutation(api.projects.createProject);
+	const archiveProject = useMutation(api.projects.archiveProject)
+	const [openDialog, setOpenDialog] = useState(false)
+
+	const onArchiveClick = async (id: Id<"projects">, isArchived: boolean) => {
+		console.log(`Archiving project with ID: ${id}, isArchived: ${isArchived}`)
+		await archiveProject({ _id: id, isArchived: !isArchived });
+		setOpenDialog(false)
+		router.push(`/projects/${id}`);
+	}
+
+	const handleExpand = (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+		event.stopPropagation();
+		onExpand?.();
+	};
+
+	const onCreate = (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+		event.stopPropagation();
+
+		const promise = create({ title: "Untitled", parentProject: id }).then(() => {
+			if (!expanded) {
+				onExpand?.();
+			}
+		}).catch((error) => {
+			console.error("Error creating project:", error);
+		});
+
+		toast.promise(promise, {
+			loading: "Creating new project...",
+			success: "New project created",
+			error: "Failed to create project",
+		});
+	};
+
+
+	const ChevronIcon = expanded ? ChevronDown : ChevronRight;
+
 	return (
 		<div
-			className="group min-h-[27px] mt-2 flex items-center text-muted-foregroundfont-medium text-sm py-1 pr-3 w-full hover:bg-primary/5"
-			style={{ paddingLeft: "12px" }}
+			className={cn(
+				"group min-h-[35px] w-full mt-2 rounded flex items-center text-gray-700 font-medium text-base py-1 pr-3 hover:bg-white/60",
+				active && "bg-primary/5 text-primary"
+			)}
+			style={{ paddingLeft: level ? `${(level * 12) + 12}px` : "12px" }}
 			onClick={onClick}
-			role="button">
-			<Icon className="shrink-0 h-[18px] mr-2 text-muted-foreground" />
+			role="button"
+		>
+			{!!id && (
+				<div
+					role="button"
+					className="h-full rounded-sm hover:bg-neutral-300 dark:bg-neutral-600 mr-1"
+					onClick={handleExpand}
+				>
+					<ChevronIcon className="h-4 w-4 shrink-0 " />
+				</div>
+			)}
+
+			{Icon && (<Icon className="shrink-0 h-[18px] mr-2 text-muted-foreground" />)}
+
+
 			<span className="truncate">{label}</span>
+
+			{!!id && (
+				<div className="ml-auto flex items-center gap-x-2">
+					<div
+						role="button"
+						onClick={() => { }} // pass onCreate function to enable creating nested childs
+						className="opacity-0 group-hover:opacity-100 h-full ml-auto rounded-sm hover:bg-neutral-300 dark:hover:bg-neutral-600"
+					>
+						<PlusIcon className="h-4 w-4 text-gray-500" />
+					</div>
+
+					<div className="relative flex items-center">
+						<DropdownMenu>
+							<DropdownMenuTrigger>
+								<div
+									role="button"
+									className="opacity-0 group-hover:opacity-100 h-full ml-auto rounded-sm hover:bg-neutral-300 dark:hover:bg-neutral-600"
+								>
+									<MoreHorizontal className="h-4 w-4 text-gray-500" />
+								</div>
+							</DropdownMenuTrigger>
+							<DropdownMenuContent align="end">
+								<DropdownMenuItem onClick={() => setOpenDialog(!openDialog)}>Archive</DropdownMenuItem>
+							</DropdownMenuContent>
+						</DropdownMenu>
+
+					</div>
+				</div>
+			)}
+			<Dialog open={openDialog} onOpenChange={() => setOpenDialog(!openDialog)}>
+				<DialogContent>
+					<DialogHeader>
+						<DialogTitle>Archive Project?</DialogTitle>
+						<DialogDescription>
+							Are you sure, you want to Archive this Project: <b>Project Title</b>
+						</DialogDescription>
+					</DialogHeader>
+					<DialogFooter>
+						<Button onClick={() => onArchiveClick(id!, false)}>Yes, Archive</Button>
+					</DialogFooter>
+				</DialogContent>
+			</Dialog>
 		</div>
 	);
 };
---
+++ app/(main)/_components/project-list.tsx
@@ -0,0 +1,89 @@
+'use client'
+
+import { api } from "@/convex/_generated/api";
+import { Doc, Id } from "@/convex/_generated/dataModel";
+import { useQuery } from "convex/react";
+import { useParams } from "next/navigation";
+import { useState } from "react";
+import { cn } from "@/lib/utils";
+import Item from './item'
+
+interface projectLitsProps {
+    parenProjectId?: Id<"projects">;
+    level?: number;
+    data?: Doc<"projects">[]
+}
+
+export const ProjectList = ({
+    parenProjectId,
+    level = 0,
+    data
+}: projectLitsProps) => {
+    const params = useParams()
+    // const router = useRouter()
+    const [expanded, setExpanded] = useState<Record<string, boolean>>({})
+
+    const onExpand = (projectId: string) => {
+        setExpanded(prevExpanded => ({
+            ...prevExpanded,
+            [projectId]: !prevExpanded[projectId]
+        }))
+    };
+
+    if (data === undefined) {
+        return (
+            <>
+                <Item level={level} icon={null} />
+                {level === 0 && (
+                    <>
+                        <Item level={level} icon={null} />
+                        <Item level={level} icon={null} />
+                    </>
+                )}
+
+            </>
+        )
+    }
+
+    // const onRedirect = (projectId: string) => {
+    //     router.push(`/projects/${projectId}`)
+    // }
+
+    return (
+        <>
+            <p
+                style={{
+                    paddingLeft: level ? `${(level * 12) + 25}px` : undefined
+                }}
+                className={cn(
+                    "hidden text-sm font-medium text-muted-foreground/80",
+                    expanded && "last:block",
+                    level === 0 && "hidden"
+                )}
+            >
+                No pages inside
+            </p>
+            {data.map((project) => (
+                <div key={project._id}>
+                    <Item
+                        id={project._id}
+                        onClick={() => { }}
+                        // onClick={() => onRedirect(project._id)}
+                        label={project.title}
+                        icon={null}
+                        active={params?.documentId === project._id}
+                        level={level}
+                        onExpand={() => onExpand(project._id)}
+                        expanded={expanded[project._id]}
+                    />
+                    {/* {expanded[project._id] && (
+                        <ProjectList
+                            parenProjectId={project._id}
+                            level={level + 1}
+                        />
+                    )} */}
+                </div>
+            ))}
+        </>
+    )
+}
