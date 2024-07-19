6156efab5695e4c9d1ea1046a769db3adfa81038
Yash Soni <yash.soni2737@gmail.com>
2024-07-19 17:43:22 +0530

    WIP - Made oveviewPage a reusable component

--- app/(main)/(routes)/projects/[projectId]/page.tsx
+++ app/(main)/(routes)/projects/[projectId]/page.tsx
@@ -1,10 +1,5 @@
-"use client";
-import { useQuery } from "convex/react";
-import { api } from "@/convex/_generated/api";
 import { Id } from "@/convex/_generated/dataModel";
-import Spinner from "@/components/ui/spinner";
-import ProjectLayout from "@/app/(main)/_components/projectLayout";
-import { useRouter } from "next/navigation";
+import { redirect } from "next/navigation";
 
 interface ProjectIdPageProps {
   params: {
@@ -14,21 +9,9 @@ interface ProjectIdPageProps {
 
 const ProjectIdPage = ({ params }: ProjectIdPageProps) => {
   const id = params.projectId;
-  const router = useRouter();
-  const project = useQuery(api.projects.getProjectById, {
-    projectId: id,
-  });
 
-  if (project === undefined) {
-    return <div className="flex justify-center items-center mx-auto"><Spinner /></div>;
-  }
+  redirect(`/projects/${id}/overview`)
 
-  if (project.onboarding != 0) {
-    router.push(`/projects/${id}/onboarding`);
-    return;
-  }
-
-  return <ProjectLayout project={project} />
 };
 
 export default ProjectIdPage;
