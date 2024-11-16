75a4969dc55c7213c0ae69a1a109a0ddbeb1b862
JazzyLeopard <kursad@hey.com>
2024-11-16 14:02:34 +0100

    Changes before branch rename

--- next.config.js
+++ next.config.js
@@ -1,9 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     eslint: {
-        // Warning: This allows production builds to successfully complete even if
-        // your project has ESLint errors.
-        ignoreDuringBuilds: false,
+        ignoreDuringBuilds: true,
+    },
+    reactStrictMode: true,
+    swcMinify: true,
+    images: {
+        domains: ['your-image-domains.com'],
     },
 }
 
