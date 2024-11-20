92f150933e12b88f79b12b8494a090fa5ef11c37
Yash Soni <yash.soni2737@gmail.com>
2024-11-20 12:04:39 +0530

    Landing page fixes

--- app/ConvexClientProvider.tsx
+++ app/ConvexClientProvider.tsx
@@ -1,10 +1,9 @@
 "use client";
 
-import { ReactNode } from "react";
-import { ConvexReactClient } from "convex/react";
 import { ClerkProvider, useAuth } from "@clerk/clerk-react";
+import { ConvexReactClient } from "convex/react";
 import { ConvexProviderWithClerk } from "convex/react-clerk";
-import { Toaster } from "sonner";
+import { ReactNode } from "react";
 
 const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
 
@@ -17,7 +16,6 @@ export default function ConvexClientProvider({
     <ClerkProvider
       publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY!}
     >
-      {/* <Toaster position="bottom-right" /> */}
       <ConvexProviderWithClerk client={convex} useAuth={useAuth}>
         {children}
       </ConvexProviderWithClerk>
