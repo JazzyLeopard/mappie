f942b614fe59851a59169e09dd9e52883c1d822d
Yash Soni <yash.soni2737@gmail.com>
2024-11-28 13:49:50 +0530

    undo clerk fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,6 @@ import ConvexClientProvider from "./ConvexClientProvider";
 import "./globals.css";
 
 import { Toaster } from "sonner";
-import { ClerkProvider } from "@clerk/clerk-react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -35,15 +34,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>
-        <ClerkProvider publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY!}>
-          <ConvexClientProvider>
-            <Toaster />
-            {children}
-          </ConvexClientProvider>
-        </ClerkProvider>
-      </body>
-    </html>
+    <ConvexClientProvider>
+      <html lang="en">
+        <body className={inter.className}>
+          {children}
+        </body>
+      </html>
+    </ConvexClientProvider>
   );
 }
