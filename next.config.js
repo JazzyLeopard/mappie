841536dbff766b995c7a3c9c47ebecba1691bc01
Yash Soni <yash.soni2737@gmail.com>
2025-01-08 23:04:19 +0530

    fixed chat history max tokens issue

--- next.config.js
+++ next.config.js
@@ -6,20 +6,30 @@ const nextConfig = {
     reactStrictMode: true,
     swcMinify: true,
     images: {
-        remotePatterns: [{
-            protocol: 'http',
-            hostname: 'your-image-domains.com',
-            pathname: '**',
-        }],
+        remotePatterns: [{ hostname: 'img.clerk.com' }],
+    },
+    experimental: {
+        optimizeCss: true,
+        optimizePackageImports: ['lucide-react', '@radix-ui/react-icons'],
     },
     pageExtensions: ['ts', 'tsx', 'js', 'jsx'],
-    webpack: (config, { isServer }) => {
-        config.watchOptions = {
+    webpack: (config, { dev, isServer }) => {
+        // Production optimizations
+        if (!dev && !isServer) {
+          // Enable tree shaking and minification
+          config.optimization = {
+            ...config.optimization,
+            usedExports: true,
+            sideEffects: true,
+            minimize: true,
+          }
+          config.watchOptions = {
             aggregateTimeout: 5000,
             poll: 1000,
+          }
         }
         return config
-    },
+      },
     staticPageGenerationTimeout: 60000
 }
 
